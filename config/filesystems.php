826737d1edea181fb297158cad2107b11f2461e5
hamdanherta <hamdanerbic@gmail.com>
2026-09-21 21:22:59 +0700

    fix: support cPanel public_html storage path in filesystems config

--- config/filesystems.php
+++ config/filesystems.php
@@ -40,9 +40,11 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => (is_dir(public_path('storage')) && !is_link(public_path('storage')))
-                ? public_path('storage')
-                : storage_path('app/public'),
+            'root' => is_dir(base_path('../public_html/storage')) && !is_link(base_path('../public_html/storage'))
+                ? base_path('../public_html/storage')
+                : ((is_dir(public_path('storage')) && !is_link(public_path('storage')))
+                    ? public_path('storage')
+                    : storage_path('app/public')),
             'url' => rtrim(env('APP_URL', 'http://localhost'), '/') . '/storage',
             'visibility' => 'public',
             'throw' => false,
